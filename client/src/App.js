0885ebde51eb3e7e71b9b372279e48c72e378be1
현수 <znight1020@naver.com>
2023-07-20 09:52:48 +0900

    test: connect gamepad

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TestPage from './components/TestPage';
+import { TestGamepadConnect } from './components/TestPage';
 
 function App() {
   return (
     <div className="App">
       <div>Welcome DOFBOT</div>
       <TestPage></TestPage>
+      <TestGamepadConnect></TestGamepadConnect>
     </div>
   );
 }
--- client/src/components/TestPage.js
+++ client/src/components/TestPage.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import axios from "axios"
 
-function TestPage() {
+export default function TestPage() {
     const [str, setStr] = useState("")
     useEffect(() => {
     axios.get("/api/ping").then((response) => {
@@ -13,4 +13,31 @@ function TestPage() {
   )
 }
 
-export default TestPage
+export function TestGamepadConnect() {
+  const [pad, setPad] = useState("disconnect")
+  useEffect(() => {
+    const handleGamepadConnected = (event) => {
+      const gamepad = event.gamepad;
+      console.log('Gamepad connected:', gamepad.id);
+      // Jetson Nano로 데이터 전송 및 원하는 작업 수행
+      setPad(gamepad.id)
+    };
+
+    const handleGamepadDisconnected = (event) => {
+      const gamepad = event.gamepad;
+      console.log('Gamepad disconnected:', gamepad.id);
+      // 연결 해제시 처리 내용
+      setPad("disconnect")
+    };
+
+    window.addEventListener('gamepadconnected', handleGamepadConnected);
+    window.addEventListener('gamepaddisconnected', handleGamepadDisconnected);
+
+    return () => {
+      window.removeEventListener('gamepadconnected', handleGamepadConnected);
+      window.removeEventListener('gamepaddisconnected', handleGamepadDisconnected);
+    };
+  }, [pad]);
+
+  return <div>{pad}</div>;
+}
