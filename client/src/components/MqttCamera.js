b346f62bfdbb699f89aee297eed4e9129142adf0
현수 <znight1020@naver.com>
2023-07-31 17:58:48 +0900

    test: MQTT 웹소켓을 이용한 DOFBOT 스트리밍 데이터 수신

--- client/src/components/MqttCamera.js
+++ client/src/components/MqttCamera.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import Paho, { Client } from 'paho-mqtt'
+import Paho from 'paho-mqtt';
 
 function MqttCamera() {
   const [mqttClient, setMqttClient] = useState(null);
-  const reconnectTimeout = 2000;  
+  const [imageData, setImageData] = useState(''); // 이미지 데이터를 저장할 상태 변수
+  const reconnectTimeout = 2000;
   const host = '129.254.174.120';
   const port = 1883;
 
@@ -16,10 +17,10 @@ function MqttCamera() {
     };
   }, []);
 
-    // 콜백 함수
-    const onConnect = () => {
+  // 콜백 함수
+  const onConnect = () => {
     console.log('접속완료');
-    mqttClient.subscribe('web');
+    mqttClient.subscribe('jetson/camera');
     const message = new Paho.MQTT.Message('start');
     // topic 설정
     message.destinationName = 'jetson/camera';
@@ -33,15 +34,17 @@ function MqttCamera() {
   };
 
   const onMessageArrived = (msg) => {
-    let out_msg = `메시지전달: ${msg.payloadString}<br/>`;
-    out_msg = out_msg + `msg topic: ${msg.destinationName}`;
-    console.log(out_msg);
+    // 이미지 데이터가 Base64로 인코딩되어 있다고 가정
+    const receivedImageBase64 = msg.payloadString;
+    console.log(receivedImageBase64)
+    setImageData(receivedImageBase64); // 받은 이미지 데이터를 imageData 상태에 저장
   };
+  
 
   const MQTTconnect = () => {
     console.log(`mqtt접속: ${host}, ${port}`);
     // 클라이언트 오브젝트 생성
-    const client = new Paho.MQTT.Client(host, port, 'react_client');
+    const client = new Paho.Client(host, port, 'react_client');
     const options = {
       timeout: 3,
       onSuccess: onConnect,
@@ -50,14 +53,16 @@ function MqttCamera() {
     client.onMessageArrived = onMessageArrived;
     client.connect(options); // connect
     setMqttClient(client);
+    console.log("클라이언트 생성")
   };
 
   return (
     <div>
       <h1>MQTT와 웹소켓 테스트</h1>
-      <img src="" id="myimg" />
+      {/* imageData 상태에 있는 이미지 데이터를 img 태그의 src 속성에 지정 */}
+      <img src={`data:image/jpeg;base64, ${imageData}`} id="myimg" />
     </div>
   );
-};
+}
 
 export default MqttCamera;
