bb1910664f4dbf2683a3cba110e34ed07f62d28f
현수 <znight1020@naver.com>
2023-08-24 09:35:59 +0900

    fix: baseurl 수정

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/components/layout/main.css';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import SmartArmDashboard from './components/smart_arm/SmartArmDashboard';
 import SmartFarmDashboard from './components/smart_farm/SmartFarmDashboard';
@@ -13,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/smart_arm" element={<SmartArmDashboard />} />
                 <Route path="/smart_farm" element={<SmartFarmDashboard />} />
+                <Route path="/" element={<Navigate to="/smart_farm" />} />
             </Routes>
         </div>
     );
